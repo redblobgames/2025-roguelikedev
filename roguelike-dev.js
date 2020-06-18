dd1a3d26a1635bbd21d5e76f1fd3c939119ec065
Amit Patel <redblobgames@gmail.com>
2020-06-18 15:51:53 -0700

    Initial checkin - bring up a console using rot.js

---
+++ roguelike-dev.js
@@ -0,0 +1,14 @@
+/*
+ * From https://www.redblobgames.com/x/2025-roguelike-dev/
+ * Copyright 2020 Red Blob Games <redblobgames@gmail.com>
+ * License: Apache-2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+ *
+ * rot.js licensed under BSD 3-Clause "New" or "Revised" License
+ * <https://github.com/ondras/rot.js/blob/master/license.txt>
+ */
+'use strict';
+
+/* global ROT */
+
+const display = new ROT.Display({width: 40, height: 25, fontFamily: 'Roboto Mono'});
+document.getElementById('container').appendChild(display.getContainer());
