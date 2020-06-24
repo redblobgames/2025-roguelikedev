0c9ac5b4b5cca042a243f6ac03f6b2369c3b078c
Amit Patel <redblobgames@gmail.com>
2020-06-24 16:57:21 -0700

    Add dungeon map, restrict movement to not pass through walls

--- roguelike-dev.js
+++ roguelike-dev.js
@@ -10,18 +10,12 @@
 
 /* global ROT */
 
+ROT.RNG.setSeed(123456);
 const display = new ROT.Display({width: 60, height: 25, fontFamily: 'Roboto Mono'});
 document.getElementById("game").appendChild(display.getContainer());
 
-function setupKeyboardHandler(display, handler) {
-    const canvas = display.getContainer();
-    const focusReminder = document.getElementById('focus-reminder');
-    canvas.setAttribute('tabindex', "1");
-    canvas.addEventListener('keydown', handleKeyDown);
-    canvas.addEventListener('blur', () => { focusReminder.style.visibility = 'visible'; });
-    canvas.addEventListener('focus', () => { focusReminder.style.visibility = 'hidden'; });
-    canvas.focus();
-}
+
+
 
 let entities = new Map();
 function createEntity(type, x, y) {
@@ -32,8 +26,26 @@ function createEntity(type, x, y) {
 }
 createEntity.id = 0;
 
-let player = createEntity('player', 5, 4);
-createEntity('troll', 20, 10);
+let player = createEntity('player', 5, 5);
+createEntity('troll', 17, 10);
+
+
+
+function createMap(width, height) {
+    let map = {
+        width, height,
+        tiles: new Map(),
+        key(x, y) { return `${x},${y}`; },
+        get(x, y) { return this.tiles.get(this.key(x, y)); },
+        set(x, y, value) { this.tiles.set(this.key(x, y), value); },
+    };
+
+    const digger = new ROT.Map.Digger(width, height);
+    digger.create((x, y, contents) => map.set(x, y, contents));
+    return map;
+}
+let map = createMap(60, 25);
+
 
 function drawEntity(entity) {
     const visuals = {
@@ -48,6 +60,15 @@ function drawEntity(entity) {
 
 function draw() {
     display.clear();
+    for (let y = 0; y < map.height; y++) {
+        for (let x = 0; x < map.width; x++) {
+            if (map.get(x, y)) {
+                display.draw(x, y, '⨉', "hsl(60, 10%, 40%)", "gray");
+            } else {
+                display.draw(x, y, '·', "hsl(60, 50%, 50%)", "black");
+            }
+        }
+    }
     for (let entity of entities.values()) {
         drawEntity(entity);
     }
@@ -69,14 +90,28 @@ function handleKeyDown(event) {
     if (action) {
         if (action[0] === 'move') {
             let [_, dx, dy] = action;
-            player.x += dx;
-            player.y += dy;
+            let newX = player.x + dx,
+                newY = player.y + dy;
+            if (map.get(newX, newY) === 0) {
+                player.x = newX;
+                player.y = newY;
+            }
             draw();
         } else {
             throw `unhandled action ${action}`;
         }
         event.preventDefault();
     }
+}
+
+function setupKeyboardHandler(display, handler) {
+    const canvas = display.getContainer();
+    const focusReminder = document.getElementById('focus-reminder');
+    canvas.setAttribute('tabindex', "1");
+    canvas.addEventListener('keydown', handleKeyDown);
+    canvas.addEventListener('blur', () => { focusReminder.style.visibility = 'visible'; });
+    canvas.addEventListener('focus', () => { focusReminder.style.visibility = 'hidden'; });
+    canvas.focus();
 }
 
 draw();
