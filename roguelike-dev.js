b2e731a65017df8562afd6885e28f5c4301d713c
Amit Patel <redblobgames@gmail.com>
2020-07-01 16:56:28 -0700

    Add field of view
    * Use ROT.js field of view algorithm.
    * Draw the map by using background color from map (wall + lighting)
      and foreground color + character from entity.

--- roguelike-dev.js
+++ roguelike-dev.js
@@ -10,7 +10,7 @@
 
 /* global ROT */
 
-ROT.RNG.setSeed(123456);
+ROT.RNG.setSeed(127);
 const display = new ROT.Display({width: 60, height: 25, fontFamily: 'Roboto Mono'});
 document.getElementById("game").appendChild(display.getContainer());
 
@@ -26,8 +26,8 @@ function createEntity(type, x, y) {
 }
 createEntity.id = 0;
 
-let player = createEntity('player', 5, 5);
-createEntity('troll', 17, 10);
+let player = createEntity('player', 1, 5);
+createEntity('troll', 24, 16);
 
 
 
@@ -46,31 +46,50 @@ function createMap(width, height) {
 }
 let map = createMap(60, 25);
 
+const fov = new ROT.FOV.PreciseShadowcasting((x, y) => map.get(x, y) === 0);
 
-function drawEntity(entity) {
+/** return [char, fg, optional bg] for a given entity */
+function entityGlyph(entityType) {
     const visuals = {
-        player: ['@', "hsl(60, 100%, 50%)"],
-        troll: ['T', "hsl(120, 60%, 50%)"],
-        orc: ['o', "hsl(100, 30%, 50%)"],
+        player: ['@', "hsl(60, 100%, 70%)"],
+        troll: ['T', "hsl(120, 60%, 30%)"],
+        orc: ['o', "hsl(100, 30%, 40%)"],
     };
-
-    const [ch, fg, bg] = visuals[entity.type];
-    display.draw(entity.x, entity.y, ch, fg, bg);
+    return visuals[entityType];
 }
 
 function draw() {
     display.clear();
+
+    let lightMap = new Map(); // map key to 0.0–1.0
+    fov.compute(player.x, player.y, 10, (x, y, r, visibility) => {
+        lightMap.set(map.key(x, y), visibility);
+    });
+
+    let glyphMap = new Map(); // map key to [char, fg, optional bg]
+    for (let entity of entities.values()) {
+        glyphMap.set(map.key(entity.x, entity.y), entityGlyph(entity.type));
+    }
+    
+    const mapColors = {
+        [false]: {[false]: "rgb(50, 50, 150)", [true]: "rgb(0, 0, 100)"},
+        [true]: {[false]: "rgb(200, 180, 50)", [true]: "rgb(130, 110, 50)"}
+    };
     for (let y = 0; y < map.height; y++) {
         for (let x = 0; x < map.width; x++) {
-            if (map.get(x, y)) {
-                display.draw(x, y, '⨉', "hsl(60, 10%, 40%)", "gray");
-            } else {
-                display.draw(x, y, '·', "hsl(60, 50%, 50%)", "black");
+            let lit = lightMap.get(map.key(x, y)) > 0.0,
+                wall = map.get(x, y) !== 0;
+            let ch = ' ',
+                fg = "black",
+                bg = mapColors[lit][wall];
+            let glyph = glyphMap.get(map.key(x, y));
+            if (glyph) {
+                ch = lit? glyph[0] : ch;
+                fg = glyph[1];
+                bg = glyph[2] || bg;
             }
+            display.draw(x, y, ch, fg, bg);
         }
-    }
-    for (let entity of entities.values()) {
-        drawEntity(entity);
     }
 }
 
